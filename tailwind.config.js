dab3b3cda43cdb65bc54be5c9c06c6cabdc3df8d
Anshika <anshikathakur596@gmail.com>
2022-07-24 22:41:23 +0530

    tailwind  config updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'hero-pattern': "linear-gradient(to right bottom, rgba(0,0,0, 0.7), rgba(0,0,0, 0.7)), url('https://images.unsplash.com/photo-1526667383715-3c42cbae3d60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')",
+      background: {
+        'heroPattern': "linear-gradient(to right bottom, rgba(0,0,0, 0.7), rgba(0,0,0, 0.7)), url('https://images.unsplash.com/photo-1589227365533-cee630bd59bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')",
       },
       fontFamily: {
         'Exo': ['Exo', 'sans-serif'],
